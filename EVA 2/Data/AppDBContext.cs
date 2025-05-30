f4477c13b9fdefafea590b013c6600ce2fd82b9a
ssj12 <ssj12@RAIDER>
2025-05-30 17:55:35 -0400

    alooooo

---
+++ EVA 2/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EVA_2.Models;
+
+
+namespace EVA_2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+
+    }
+
+}
