a165deb4c921d5b0485bbab5f762a0b87880e73f
Fluxzify <josevelasquezguerreroo@gmail.com>
2025-05-31 20:39:06 -0400

    AAAd

--- EVA 2/Models/Cita.cs
+++ EVA 2/Models/Cita.cs
@@ -5,13 +5,17 @@
         public int Id { get; set; }
 
         public int ClienteId { get; set; }
-        public Cliente Cliente { get; set; } = null!;
+        public Cliente? Cliente { get; set; } = null!;
 
         public int ServicioId { get; set; }
-        public Servicio Servicio { get; set; } = null!;
+        public Servicio? Servicio { get; set; } = null!;
+
+    public int EstadoId { get; set; }            // Clave foránea
+    public Estado Estado { get; set; } = null!;  // Propiedad de navegación
+
         public DateTime Fecha { get; set; }
         public TimeSpan Hora { get; set; }
-
+        public string Estado { get; set; } = null!; // "Pendiente", "Confirmada", "Completada", "Cancelada", "Mas texto"
         public string? Comentarios { get; set; }
     }
 }
