1776ee0d816c8b402a0c708ba619af32de65d52b
JUAN <j.melipillan@alumnos.santotomas.cl>
2025-06-02 20:50:10 -0400

    listooo

---
+++ EVA 2/Models/ReportesPorPeriodo.cs
@@ -0,0 +1,11 @@
+﻿namespace EVA_2.Models
+{
+    public class ReportesPorPeriodo
+    {
+        public DateTime Fecha { get; set; }
+        public string Hora { get; set; }
+        public string Cliente { get; set; }
+        public string Servicio { get; set; }
+        public string Estado { get; set; }
+    }
+}
